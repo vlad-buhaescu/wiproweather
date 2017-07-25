200c7cc72948cf4a2e20dd9cdc3b7ebc298d8e83
Vlad-Constantin Buhaescu <buhaescuvlad@yahoo.com>
2017-07-25 14:10:41 +0100

    populated the cells

---
+++ WiproWeather/ForecastCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastCollectionViewCell.swift
+//  WiproWeather
+//
+//  Created by Vlad-Constantin Buhaescu on 25/07/2017.
+//  Copyright © 2017 Vlad-Constantin Buhaescu. All rights reserved.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+    
+}
